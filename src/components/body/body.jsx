46f9487f6a54626a31212bc23a33eb947457ce4c
Priyanshu <priyanshusingh.0601@gmai.com>
2024-10-29 21:54:22 +0530

    update navbar and body

--- src/components/body/body.jsx
+++ src/components/body/body.jsx
@@ -1,24 +1,31 @@
 import './body.css'
 function body() {
     return( 
-        <section>
+        <>
               <div className='main'>
-                <div className='headtext'>
+                <div className='maintext'>
                   <p>We are a team dedicated to building
                     innovative startups, connecting talent, and delivering customized client
                     solutions.
                   </p>
                 </div>
+                <div className='subtext'>
+                  <p>We are a team dedicated to building
+                      innovative startups, connecting talent, and delivering customized client
+                      solutions.
+                  </p>
+                </div>
                 <div className='btn'>
                   <a><button className='button'>View Our Work<div className="hoverEffect">
-        <div></div>
-      </div></button></a>
+                  <div></div>
+                  </div>
+                  </button></a>
                   <a><button className='button'>Join Us<div className="hoverEffect">
-        <div></div>
-      </div></button ></a>
+                  <div></div>
+                  </div></button ></a>
                 </div>
               </div>
-            </section>
+            </>
     )
 } ;
 
--- src/components/navabar/navbar.jsx
+++ src/components/navabar/navbar.jsx
@@ -1,6 +1,6 @@
 import './navbar.css';
 import logo from '../../img/synergz-logo.png';
-import aboutUs from '../aboutUs/aboutUs';
+
 
 function navbar() {
     return (
@@ -10,16 +10,15 @@ function navbar() {
             <div className="logo">
               <a href="/aboutUs"><img src={logo} alt='logo'></img></a>
             </div > 
-              <div className='links'>
-              <button className='nav-btn bttn'>About Us</button>
-              <button className='nav-btn'>About Us</button>
-              <button className='nav-btn'>About Us</button>
-              <button className='nav-btn'>About Us</button>
-              
-    
-              </div>
-          </div>                    
-        </div>
+                <div className='links'>
+                  <a href='/s'><button className='nav-btn bttn'>About Us</button></a>
+                  <a href='/'><button className='nav-btn bttn'>Our Work</button></a>
+                  <a href='/'><button className='nav-btn bttn'>Services</button></a>
+                  <a href='/'><button className='nav-btn bttn'>Careers</button></a>
+                  <a href='/'><button className='nav-btn bttn'>Contact Us</button></a>
+                </div>
+            </div>                    
+          </div>
         </div>
       );
 }
