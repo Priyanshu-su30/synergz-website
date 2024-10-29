0544141e864176ec1d5e92d193d45a4a4c7a9b33
Priyanshu <priyanshusingh.0601@gmai.com>
2024-10-29 21:54:39 +0530

    add services to home page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Navbar from "./components/navabar/navbar";
 import Body from "./components/body/body";
-
+import AboutUs from './components/aboutUs/aboutUs'
+import Services from './services/services';
 
 function App() {
   return(
-    <div>
+    <>
       <Navbar/>
       <Body/>
-      {/* <Routes>
-          <Route path="/aboutUs" element={<AboutUs />} />
-      </Routes> */}
-    </div>
+      <AboutUs/>
+      <Services/>
+
+    </>
   )
 }
 
--- src/components/aboutUs/aboutUs.jsx
+++ src/components/aboutUs/aboutUs.jsx
@@ -1,22 +1,10 @@
 import React from 'react';
-import logo from '../../img/synergz-logo.png';
 import './aboutUs.css'
 function aboutUs(){
-    <div>
-        <div id="header">
-        <div className="logo">
-          <a href="#"><img src={logo} alt='logo'></img></a>
-        </div > 
-          <div className='links'>
-          <a href="/aboutUs">About Us</a>
-          <a href="/aboutUs">Our Work</a>
-          <a href="/aboutUs">Services</a>
-          <a href="/aboutUs">Careers</a>
-          <a href="/aboutUs">Contact Us</a>
-
-          </div>
-      </div>
-    </div>
+    <>
+    
+        
+    </>
 }
 
 export default aboutUs;
