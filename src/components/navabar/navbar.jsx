5ad87c9e63f3f0005871ff8ed3c05b294033b0d5
Priyanshu <priyanshusingh.0601@gmai.com>
2024-10-30 12:43:48 +0530

    add responsiveness to navbar

--- src/components/navabar/navbar.jsx
+++ src/components/navabar/navbar.jsx
@@ -1,26 +1,33 @@
+import React, { useState } from 'react';
 import './navbar.css';
 import logo from '../img/synergz-logo.png';
 
+function Navbar() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-function navbar() {
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
-        <div>
-          <div className='body'>
-          <div id="header">
-            <div className="logo">
-              <a href="/aboutUs"><img src={logo} alt='logo'></img></a>
-            </div > 
-                <div className='links'>
-                  <a href='/s'><button className='nav-btn bttn'>About Us</button></a>
-                  <a href='/'><button className='nav-btn bttn'>Our Work</button></a>
-                  <a href='/'><button className='nav-btn bttn'>Services</button></a>
-                  <a href='/'><button className='nav-btn bttn'>Careers</button></a>
-                  <a href='/'><button className='nav-btn bttn'>Contact Us</button></a>
+        <div className='body'>
+            <div id="header">
+                <div className="logo">
+                    <a href="/aboutUs"><img src={logo} alt='logo' /></a>
                 </div>
-            </div>                    
-          </div>
+                <div className={`links ${isMenuOpen ? 'open' : ''}`}>
+                    <a href='/aboutUs'><button className='nav-btn bttn'>About Us</button></a>
+                    <a href='/'><button className='nav-btn bttn'>Our Work</button></a>
+                    <a href='/'><button className='nav-btn bttn'>Services</button></a>
+                    <a href='/'><button className='nav-btn bttn'>Careers</button></a>
+                    <a href='/'><button className='nav-btn bttn'>Contact Us</button></a>
+                </div>
+                <div className="menu-icon" onClick={toggleMenu}>
+                    ☰
+                </div>
+            </div>
         </div>
-      );
+    );
 }
 
-export default navbar;
+export default Navbar;
